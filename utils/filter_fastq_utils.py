37f88597e0cfccea837c50236622248380d9478c
vvbasova <vvvbasovavvv@gmail.com>
2024-10-03 17:16:44 +0300

    Create script for filter_fastq

---
+++ utils/filter_fastq_utils.py
@@ -0,0 +1,53 @@
+from typing import Union, Dict, Tuple
+
+
+def filter_length(seqs: Dict[str, Tuple[str, str]],
+                  length_bounds: Tuple[int, int]
+                  ) -> Dict[str, Tuple[str, str]]:
+    filtered_length_seqs = {}
+
+    for name, (sequence, quality) in seqs.items():
+        if length_bounds[0] <= len(sequence) <= length_bounds[1]:
+            filtered_length_seqs[name] = (sequence, quality)
+
+    return filtered_length_seqs
+
+
+def gc_counter(sequence: str) -> Union[int, float]:
+    g_content = sequence.upper().count('G')
+    c_content = sequence.upper().count('C')
+    sequence_length = len(sequence)
+    gc_percentage = (g_content + c_content) / sequence_length * 100
+
+    return gc_percentage
+
+
+def filter_gc(seqs: Dict[str, Tuple[str, str]],
+              gc_bounds: Tuple[Union[int, float], Union[int, float]]
+              ) -> Dict[str, Tuple[str, str]]:
+    filtered_gc_seqs = {}
+
+    for name, (sequence, quality) in seqs.items():
+        if gc_bounds[0] <= gc_counter(sequence) <= gc_bounds[1]:
+            filtered_gc_seqs[name] = (sequence, quality)
+
+    return filtered_gc_seqs
+
+
+def quality_counter(quality: str) -> Union[int, float]:
+    q_score = [ord(symbol) - 33 for symbol in quality]
+    average_q_score = sum(q_score) / len(q_score)
+
+    return average_q_score
+
+
+def filter_quality(seqs: Dict[str, Tuple[str, str]],
+                   quality_threshold: Union[int, float]
+                   ) -> Dict[str, Tuple[str, str]]:
+    filtered_quality_seqs = {}
+
+    for name, (sequence, quality) in seqs.items():
+        if quality_threshold <= quality_counter(quality):
+            filtered_quality_seqs[name] = (sequence, quality)
+
+    return filtered_quality_seqs
